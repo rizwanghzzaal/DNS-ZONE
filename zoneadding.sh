30e2aed276c7f9a1fe0b2a67cf712d189212ef56
rizwanghzzaal <sp15bscs069@vcomsats.edu.pk>
2017-05-17 11:06:07 +0500

    Create zoneadding.sh
    adding additional zones to zone file

---
+++ zoneadding.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+replace "ns1.futuresouls.com" "webs01.futuresouls.com" --  /var/named/pakpdf.pk.db
+replace "ns2.futuresouls.com" "webs02.futuresouls.com" --  /var/named/pakpdf.pk.db
+replace "ns3.futuresouls.com" "webs03.futuresouls.com" --  /var/named/pakpdf.pk.db
+replace "ns4.futuresouls.com" "webs04.futuresouls.com" --  /var/named/pakpdf.pk.db
+replace "ns1.futuresouls.com" "webs01.futuresouls.com" --  /var/named/test.websouls.net.db
+replace "ns2.futuresouls.com" "webs02.futuresouls.com" --  /var/named/test.websouls.net.db
+replace "ns3.futuresouls.com" "webs03.futuresouls.com" --  /var/named/test.websouls.net.db
+replace "ns4.futuresouls.com" "webs04.futuresouls.com" --  /var/named/test.websouls.net.db
+replace "ns1.futuresouls.com" "webs01.futuresouls.com" --  /var/named/fb.livesport99.com.db
+replace "ns2.futuresouls.com" "webs02.futuresouls.com" --  /var/named/fb.livesport99.com.db
+replace "ns3.futuresouls.com" "webs03.futuresouls.com" --  /var/named/fb.livesport99.com.db
+replace "ns4.futuresouls.com" "webs04.futuresouls.com" --  /var/named/fb.livesport99.com.db
